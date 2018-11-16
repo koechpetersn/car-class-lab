ae39d644f24480a99dec782efd7173fea733a6cf
koech <koechpetersn@gmail.com>
2018-11-16 19:07:02 +0300

    Created car file to instantiate cars

--- car.py
+++ car.py
@@ -1,5 +1,5 @@
 class Car():
-    def __init__(self,car_type,num_of_doors,num_of_wheels=4,name = 'General',model = 'GM',speed = 0,):
+    def __init__(self,name = 'General',model = 'GM',car_type = 'saloon',num_of_doors=4,num_of_wheels=4,speed = 0,):
         self.name = name
         self.model = model
         self.car_type = car_type
@@ -8,24 +8,26 @@ class Car():
         self.speed = speed
 
     def car_doors(self):
-      if self.name == "Porsche" or self.name == "Koenigsegg" or self.car_type != 'saloon':
+      
+      if self.name == "Porshe" or self.name == "Koenigsegg" or self.car_type == 'trailer':
         num_of_doors = 2
-        return 'The number of doors is %d'%num_of_doors
+        return num_of_doors
+        
         
       else:
         num_of_doors = 4
-        return 'The number of doors is %d'%num_of_doors
-
+        return num_of_doors
+        
 
 
     def car_wheels(self):
       if self.car_type == "saloon":
         no_of_wheels = 4
-        return 'The number of wheels is %d'%no_of_wheels
+        return no_of_wheels
             
       else:
         no_of_wheels = 8
-        return 'The number of wheels is %d'%no_of_wheels
+        return no_of_wheels
     
     def is_saloon(self):
         if self.car_type == "saloon":
@@ -34,12 +36,32 @@ class Car():
             self.car_type = 'It is a Trailer'
             return False,self.car_type
 
-    def drive(self,moving_speed):
+    def car_speed(self,moving_speed):
         self.moving_speed = moving_speed
         parked_speed = self.speed
         
-        total_speed = parked_speed + moving_speed
-        return 'The moving speed is %d'%total_speed
+        total_speed = parked_speed + self.moving_speed
+        return total_speed
+
+    def drive(self,moving_speed):
+        self.moving_speed = moving_speed
+        return self
+
+'''
+opel = Car('Opel', 'Omega 3')
+porshe = Car('Porshe', '911 Turbo')
+print(opel.car_doors())
+print(porshe.car_doors())
+
+man = Car('MAN', 'Truck', 'trailer')
+koenigsegg = Car('Koenigsegg', 'Agera R')
+print(man.car_wheels())
+print(koenigsegg.car_wheels())
+
+man = Car('MAN', 'Truck', 'trailer')
+print(man.car_speed(7))
+'''
+
 
 
 
